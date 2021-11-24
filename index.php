34803ff0d4a3310ef5ec35da34f0b95e4fe0d019
alqa22 <94571045+alqa22@users.noreply.github.com>
2021-11-25 02:29:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
                     $sender = $_POST['sender'];
 		    $headers = "MIME-Version: 1.0" . "\r\n";
 		    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-			$headers .= "From: Apple <$sender>";
+			$headers .= "From:    <$sender>";
 
 
                    
